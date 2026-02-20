e453d985658f84aa8c97ab4d30c27a26d3432f80
cyrilpointet <pointet.cyril@gmail.com>
2026-02-20 18:22:32 +0100

    logout on 401

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -12,4 +12,19 @@ apiClient.interceptors.request.use((config) => {
   return config
 })
 
+apiClient.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    if (
+      error.response?.status === 401 &&
+      !error.config?.url?.startsWith("/auth/")
+    ) {
+      localStorage.removeItem("token")
+      localStorage.removeItem("user")
+      window.location.href = "/login"
+    }
+    return Promise.reject(error)
+  },
+)
+
 export default apiClient
